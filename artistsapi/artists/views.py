e988b4b1ed428704a9a9e8be928913b685effcad
Manu Grandio <manugrandio@fastmail.fm>
2023-08-23 08:37:20 +0200

    don't use viewset

--- artistsapi/artists/views.py
+++ artistsapi/artists/views.py
@@ -1,10 +1,14 @@
-from rest_framework import viewsets
+from rest_framework import mixins
+from rest_framework import generics
 from rest_framework import permissions
 
 from .serializers import ArtistsSerializer
 from .models.artists import Artists
 
 
-class ArtistsViewSet(viewsets.ModelViewSet):
+class ArtistList(mixins.ListModelMixin, generics.GenericAPIView):
     queryset = Artists.objects.all()
     serializer_class = ArtistsSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,7 @@
 from django.urls import include, path
 
-from rest_framework import routers
-
 from artistsapi.artists import views
 
-router = routers.DefaultRouter()
-router.register(r"artists", views.ArtistsViewSet)
-
 urlpatterns = [
-    path("", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("artists/", views.ArtistList.as_view()),
 ]
