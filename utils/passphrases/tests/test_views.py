98f94ae7ceb06ba48812fe199cf19add03d0f138
Manu Grandío <manugrandio@fastmail.fm>
2023-08-24 19:01:10 +0200

    Endpoint passphrase advanced (#8)

---
+++ utils/passphrases/tests/test_views.py
@@ -0,0 +1,57 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+
+class TestBasicPassphrase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create_user("john", "john@mail.com", "password")
+
+    def test_basic_passphrase(self):
+        self.client.login(username="john", password="password")
+
+        passphrases = "aa bb cc\naa bb aa\naa bb dd"
+
+        response = self.client.post("/passphrase/basic/", {"passphrases": passphrases})
+
+        self.assertEqual(response.json()["count"], 2)
+
+    def test_basic_passphrase_invalid(self):
+        self.client.login(username="john", password="password")
+
+        passphrases = ""
+
+        response = self.client.post("/passphrase/basic/", {"passphrases": passphrases})
+
+        self.assertEqual(response.status_code, 400)
+
+
+class TestAdvancedPassphrase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create_user("john", "john@mail.com", "password")
+
+    def test_advanced_passphrase(self):
+        self.client.login(username="john", password="password")
+
+        passphrases = (
+            "abcde fghij\n"  # Valid
+            "abcde xyz ecdab\n"  # Invalid
+            "a ab abc abd abf abj\n"  # Valid
+            "iiii oiii ooii oooi oooo"  # Valid
+        )
+
+        response = self.client.post(
+            "/passphrase/advanced/", {"passphrases": passphrases}
+        )
+
+        self.assertEqual(response.json()["count"], 3)
+
+    def test_advanced_passphrase_invalid(self):
+        self.client.login(username="john", password="password")
+
+        passphrases = ""
+
+        response = self.client.post("/passphrase/basic/", {"passphrases": passphrases})
+
+        self.assertEqual(response.status_code, 400)
---
+++ utils/passphrases/views.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import PassphraseSerializer
+from .passphrases import count_valid_passphrases
+
+
+class BasicPassphrase(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = PassphraseSerializer(data=self.request.data)
+        serializer.is_valid(raise_exception=True)
+        count = count_valid_passphrases(serializer.validated_data["passphrases"])
+        return Response({"count": count})
+
+
+class AdvancedPassphrase(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = PassphraseSerializer(data=self.request.data)
+        serializer.is_valid(raise_exception=True)
+        count = count_valid_passphrases(
+            serializer.validated_data["passphrases"], use_advanced=True
+        )
+        return Response({"count": count})
